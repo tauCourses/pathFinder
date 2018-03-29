8ec31d980a5a49d01b8bed29913d847027cd3d52
subdivision <eug@post.com>
2018-03-30 02:37:12 +0300

    Definining Arrangement. Checking whether a path exists.

--- CGAL_defines.h
+++ CGAL_defines.h
@@ -13,6 +13,8 @@
 #include <CGAL/Polygon_2.h>
 #include <CGAL/minkowski_sum_2.h>
 #include <CGAL/Arrangement_2.h>
+#include <CGAL/centroid.h>
+#include <CGAL/Arr_batched_point_location.h>
 
 
 typedef typename CGAL::Gmpq Number_type;
@@ -22,8 +24,19 @@ typedef typename Kernel::Point_2 Point_2;
 typedef typename Kernel::Segment_2 Segment_2;
 typedef typename Kernel::Vector_2 Vector_2;
 typedef typename CGAL::Polygon_2<Kernel> Polygon_2;
-typedef typename CGAL::Polygon_with_holes_2<Kernel> Polygon_with_holes_2;
-typedef CGAL::Arrangement_2<CGAL::Arr_segment_traits_2<Kernel>> Arrangement_2;
-typedef typename Polygon_2::Vertex_iterator VertexIterator;
+typedef typename CGAL::Polygon_with_holes_2<Kernel>         Polygon_with_holes_2;
+typedef typename CGAL::Polygon_with_holes_2<Kernel>::Hole_const_iterator 
+                                                            HoleCIter;
+typedef typename CGAL::Arrangement_2<CGAL::Arr_segment_traits_2<Kernel>> 
+                                                            Arrangement_2;
+typedef typename Polygon_2::Vertex_iterator                 VrtxIter;
+typedef typename Polygon_2::Vertex_const_iterator           VrtxCIter;
+typedef typename Polygon_2::Edge_const_iterator             EdgeIter;
+typedef CGAL::Arr_naive_point_location<Arrangement_2>       NaivePL;
 
+typedef CGAL::Arr_point_location_result<Arrangement_2>  PointLocationResult;
+typedef std::pair<Point_2, PointLocationResult::Type>   QueryResult;
+typedef Arrangement_2::Vertex_const_handle              ArrVrtxCHandle;
+typedef Arrangement_2::Halfedge_const_handle            ArrHedgeCHandle;
+typedef Arrangement_2::Face_const_handle                ArrFaceCHandle;
 #endif //INC_2_3_CGAL_DEFINES_H
--- main.cpp
+++ main.cpp
@@ -23,6 +23,9 @@ Polygon_2 loadPolygon(ifstream &is) {
     Polygon_2 ret;
     while (polygon_size--)
         ret.push_back(loadPoint_2(is));
+    CGAL::Orientation orient = ret.orientation();
+    if( CGAL::CLOCKWISE == orient )
+      ret.reverse_orientation();
     return ret;
 }
 
@@ -35,42 +38,144 @@ vector<Polygon_2> loadPolygons(ifstream &is) {
     return ret;
 }
 
-Polygon_2 getInverseRobot(const Polygon_2 &robot, const Point_2 &refPoint)
+//----------------------------------------------------------------------------
+void
+print_polygon( const Polygon_2& pgn )
 {
-    Polygon_2 ret;
-    for (VertexIterator vi = robot.vertices_begin(); vi != robot.vertices_end(); ++vi) {
-        //movedPoint is the robot Point moved to the new origin(ref point)
-        Point_2 movedPoint = {(*vi)[0] + refPoint[0] - robot[0][0] , (*vi)[1] + refPoint[1] - robot[0][1]};
-        ret.push_back({movedPoint[0] - 2 * (movedPoint[0] - refPoint[0]), movedPoint[1] - 2 * (movedPoint[1] - refPoint[1])});
-    }
-
-    return ret;
+  cout << pgn.size() << " ";
+  VrtxCIter vit = pgn.vertices_begin();
+  for( ; vit != pgn.vertices_end(); ++vit )
+  {
+    double x = CGAL::to_double((*vit)[0]);
+    double y = CGAL::to_double((*vit)[1]);
+    cout << x << " " << y << " ";
+  }
+  cout << endl;
 }
-vector<Point_2> findPath(const Point_2 &start,
-                         const Point_2 &end,
-                         const Polygon_2 &robot,
-                         vector<Polygon_2> &obstacles) {
-    Polygon_2 inverseRobot = getInverseRobot(robot, start);
 
-    vector<Polygon_with_holes_2> obstaclesC;
-    for(Polygon_2& obstacle: obstacles)
-        obstaclesC.push_back(CGAL::minkowski_sum_2(inverseRobot, obstacle));
-
-    cout << "first obstacleC " << obstaclesC[0] << endl;
-    Arrangement_2 arr; //is it the right data structure?
-    insert(arr, obstaclesC[0]); // not working!
-    //create trapzohid
-    //createGraph
-    //findPathInGraph
-    //return pathToPoints
-
-
-    cout << "inverseRobot " << inverseRobot << endl;
-    cout << "start - " << start << endl;
-    cout << "first robot point " << robot[0] << endl;
-    cout << "end " << end << endl;
-    return vector<Point_2>({start,{1.71,5.57},{23.84,5.94},{21.21,29.17}, end});
+//----------------------------------------------------------------------------
+void
+print_polygon( const Polygon_with_holes_2& pwh )
+{
+  if( !pwh.is_unbounded())
+  {
+    cout << "{ Outer boundary " << endl;
+    print_polygon (pwh.outer_boundary());
+  }
+  else
+    cout << "{ Unbounded polygon." << endl;
+  unsigned int k = 1;
+  std::cout << pwh.number_of_holes() << " holes:" << std::endl;
+  for (HoleCIter hit = pwh.holes_begin(); hit != pwh.holes_end(); ++hit, ++k)
+  {
+    cout << " Hole #" << k << " = " << endl;
+    print_polygon (*hit);
+  }
+  cout << "}" << endl;
 }
+
+//----------------------------------------------------------------------------
+Polygon_2 getInversedRobot( const Polygon_2& robot,
+                            const Point_2&   refPoint )
+{
+  Polygon_2 ret;
+  VrtxCIter vi = robot.vertices_begin();
+  for( ; vi != robot.vertices_end(); ++vi)
+  {
+    //movedPoint is the robot Point moved to the new origin(ref point)
+    Point_2 movedPoint = { (*vi)[0] + refPoint[0] - robot[0][0],
+                           (*vi)[1] + refPoint[1] - robot[0][1] };
+    ret.push_back( { movedPoint[0] - 2 * (movedPoint[0] - refPoint[0]),
+                     movedPoint[1] - 2 * (movedPoint[1] - refPoint[1])});
+  }
+  CGAL::Orientation orient = ret.orientation();
+  if( CGAL::CLOCKWISE == orient )
+    ret.reverse_orientation();
+  return ret;
+}
+
+//----------------------------------------------------------------------------
+bool insertPolygonIntoArng( Arrangement_2&              arr,
+                            const Polygon_with_holes_2& pwh )
+{
+  if( pwh.is_unbounded() )
+    return false;
+
+  const Polygon_2& plg = pwh.outer_boundary();
+  CGAL::insert(arr, plg.edges_begin(), plg.edges_end() );
+  for( HoleCIter hit = pwh.holes_begin(); hit != pwh.holes_end(); ++hit )
+  {
+    const Polygon_2& plg = *hit;
+    CGAL::insert(arr, plg.edges_begin(), plg.edges_end() );
+  }
+  return true;
+}
+
+//----------------------------------------------------------------------------
+bool getPathFace( const Point_2&          start,
+                  const Point_2&          end,
+                  const Arrangement_2&    arr)
+                  //const ArrFaceCHandle*&  phArrFace )
+{
+  list<Point_2> pts;
+  pts.push_back(start);
+  pts.push_back(end);
+  std::list<QueryResult>  results;
+  locate(arr, pts.begin(), pts.end(), std::back_inserter(results));
+  std::list<QueryResult>::const_iterator it;
+  bool bProperFace[2];
+  int i = 0;
+  const ArrFaceCHandle* f;
+  for( it = results.begin(); it != results.end(); ++it, ++i)
+  {
+    //cout << "The point (" << it->first << ") is located ";
+    if( f = boost::get<ArrFaceCHandle>(&(it->second)) )
+      bProperFace[i] = true;
+    else
+      bProperFace[i] = false;
+  }
+  it = results.begin();
+  ++it;
+  bool bSameObject = it->second == results.begin()->second;
+  //if( bSameObject && bProperFace[1] )
+  //  cout << "inside "
+  //       << (((*f)->is_unbounded()) ? "the unbounded" : "a bounded")
+  //       << " face." << endl;
+  return bSameObject && bProperFace[1];
+}
+
+//----------------------------------------------------------------------------
+vector<Point_2> findPath(const Point_2&      start,
+                         const Point_2&      end,
+                         const Polygon_2&    robot,
+                         vector<Polygon_2>&  obstacles)
+{
+  Polygon_2 invRobot = getInversedRobot( robot, start );
+  vector<Polygon_with_holes_2> vecConfObst;
+  for(const Polygon_2& obst : obstacles )
+  {
+    Polygon_with_holes_2 currConfObst = CGAL::minkowski_sum_2( obst,
+                                                               invRobot );
+    vecConfObst.push_back( currConfObst );
+    //print_polygon(currConfObst.outer_boundary());
+  }
+
+  Arrangement_2 arr;
+  for( const Polygon_with_holes_2& pwh : vecConfObst)
+    insertPolygonIntoArng( arr, pwh );
+
+  const ArrFaceCHandle* phArrFace;
+  if( !getPathFace(start, end, arr ))//, phArrFace ) )
+    return vector<Point_2>();
+
+  cout << "Query points are in the same Arrangement face. "
+       << "A solution exists" << endl;
+
+  //... to be continued...
+  //
+  return vector<Point_2>({start,{1.71,5.57},{23.84,5.94},{21.21,29.17}, end});
+}
+//----------------------------------------------------------------------------
 
 int main(int argc, char *argv[]) {
     if (argc != 4) {
@@ -79,9 +184,12 @@ int main(int argc, char *argv[]) {
     }
 
     ifstream inputRobotFile(argv[1]), inputObstaclesFile(argv[2]);
-    if (!inputRobotFile.is_open() || !inputObstaclesFile.is_open()) {
-        if (!inputRobotFile.is_open()) cerr << "ERROR: Couldn't open file: " << argv[1] << endl;
-        if (!inputObstaclesFile.is_open()) cerr << "ERROR: Couldn't open file: " << argv[2] << endl;
+    if (!inputRobotFile.is_open() || !inputObstaclesFile.is_open())
+    {
+        if (!inputRobotFile.is_open())
+            cerr << "ERROR: Couldn't open file: " << argv[1] << endl;
+        if (!inputObstaclesFile.is_open())
+            cerr << "ERROR: Couldn't open file: " << argv[2] << endl;
         return -1;
     }
 
